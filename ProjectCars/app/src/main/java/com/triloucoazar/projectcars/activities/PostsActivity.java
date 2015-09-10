3ee7f59562932c02a5c037fdad19fdc1166015dd
Marcon <dm.marcon@gmail.com>
2015-09-10 16:12:32 -0300

    Add some style to the app

--- ProjectCars/app/src/main/java/com/triloucoazar/projectcars/activities/PostsActivity.java
+++ ProjectCars/app/src/main/java/com/triloucoazar/projectcars/activities/PostsActivity.java
@@ -1,7 +1,10 @@
 package com.triloucoazar.projectcars.activities;
 
 import android.os.Bundle;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.ActionBarActivity;
+import android.view.Menu;
+import android.view.MenuItem;
 
 import com.triloucoazar.projectcars.R;
 
@@ -11,5 +14,22 @@ public class PostsActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_posts);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_posts, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.menu_posts_search:
+
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 }
